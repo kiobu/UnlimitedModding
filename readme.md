85940f11706033c1f80abd06e122ef6b8f4a22ae
kiobu <cdolson30@gmail.com>
2021-03-19 13:55:27 -0400

    Update init and readme.

--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # UnlimitedModding
 
-This mod removes all modding restrictions for weapons and weapon mods.
+**Note:** This mod will cause your game to take significantly longer (10-15 minutes) to load. This is due to the client having to calculate all mods for all weapons and mods recursively from the server. It is recommended to use the `NoFiltersPatch` in JET instead.
--- src/init.js
+++ src/init.js
@@ -2,17 +2,31 @@ exports.mod = () => {
 	let inclusion = ["weap_", "mod"]
 	let items = fileIO.readParsed(global.db.user.cache.items)
 
-	let cnt = 0;
+	let allmods = []
 
+	// Get all mods.
 	for (let item in items.data) {
 		let curritem = items.data[item]
+		try {
+			if (inclusion.some(inc => curritem._props["ItemSound"].includes("mod"))) {
+				if (!allmods.includes(curritem._parent)) {
+					allmods.push(curritem._parent)
+				}
+			}
+		} catch (_) { } // This item is not a mod.
+	}
 
+	let cnt = 0;
+
+	// Set the filters of all weapons and mods to all mods.
+	for (let item in items.data) {
+		let curritem = items.data[item]
 		if (curritem._props.hasOwnProperty("ItemSound")) {
 			if (inclusion.some(inc => curritem._props["ItemSound"].includes(inc))) {
 				try {
 					curritem._props["Slots"].forEach((el) => {
 						try {
-							el._props.filters = [{ "Filter": [] }]
+							el._props.filters = [{ "Filter": allmods }]
 							cnt++;
 						} catch (_) {
 							return; // This item doesn't have a 'filters' array.
@@ -25,5 +39,6 @@ exports.mod = () => {
 
 	fileIO.write(global.db.user.cache.items, items) // Write our changes to the cache.
 
-	logger.logSuccess(`[MOD] UnlimitedModding enabled. Removed filters for ${cnt} items.`)
+	logger.logSuccess(`[MOD] UnlimitedModding enabled. Refiltered ${cnt} items.`)
+	logger.logWarning(`NOTE: UnlimitedModding is taxing on the client. It will cause your game to take significantly longer to load (5+ minutes).`)
 }
